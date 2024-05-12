9d867113769825bb4d6c355e7161d338e7a41fb5
Hemant <patilhemant2042@gmail.com>
2024-05-12 17:26:21 +0530

    first commit

---
+++ src/pages/about.tsx
@@ -0,0 +1,43 @@
+const About = () => {
+  return (
+    <div className="home">
+      <h1 style={{display:'flex',flexDirection:'column', marginBottom:'15px'}}>ABOUT US</h1>
+      <main>
+        <h2>
+          At EasyBuy, we're passionate about bringing the latest and most
+          innovative electronics products to tech enthusiasts and consumers
+          worldwide. With a dedicated team of experts and a commitment to
+          excellence, we strive to provide an unparalleled shopping experience,
+          offering a wide range of high-quality electronics at competitive
+          prices. Founded in 2024, EasyBuy has quickly emerged as a
+          trusted destination for electronics enthusiasts, offering a carefully
+          curated selection of products from leading brands and manufacturers.
+          Whether you're searching for the latest smartphones, cutting-edge
+          gadgets, or essential accessories, we've got you covered.
+          <br/>
+          
+          <b>Our mission
+          is simple:</b> to make the latest technology accessible to everyone, while
+          delivering exceptional value and service. With a focus on customer
+          satisfaction and continuous improvement, we're constantly innovating
+          and evolving to meet the ever-changing needs of our customers. 
+
+          <br/>
+          At
+          EasyBuy, we believe in transparency, integrity, and a
+          customer-first approach. We're committed to building long-lasting
+          relationships with our customers, providing expert advice,
+          personalized recommendations, and ongoing support every step of the
+          way. 
+          <br/>
+          <b>
+          Thank you for choosing EasyBuy for all your electronics
+          needs. We look forward to serving you and exceeding your expectations!
+          </b>
+        </h2>
+      </main>
+    </div>
+  );
+};
+
+export default About;
